294367310c79a04997f82575bd8ded41f82c813c
limingru <18813070947@163.com>
2019-10-22 08:37:10 +0800

    node server code

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
+const cors = require('cors')
+
+app.get('/', (req, res) => {
+  res.send('hello nodejs')
+})
+
+app.options('/upload', cors())
+app.post('/upload', cors(), upload.single('file'), (req, res, next) => {
+  // res.header('Access-Control-Allow-Origin', '*')
+  res.send(req.file.filename)
+})
+
+app.get('/preview/:key', cors(), (req, res) => {
+  return res.sendFile(`uploads/${req.params.key}`, {
+    root: __dirname,
+    headers: {
+      'Content-Type': 'image/jpeg'
+    }
+  })
+})
+
+app.listen(3000)
